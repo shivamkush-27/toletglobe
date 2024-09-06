568a4fb53dffb442706264b23197c52694f874db
Mriganka Das <dasmriganka.2002@gmail.com>
2024-09-06 14:48:07 +0530

    feature:Responsiveness is implemented  in both header and home page
    co-powerd-by: Mriganka Das
    <dasmriganka.2002@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <header className="bg-black py-4">
       <nav className="container mx-auto px-4 flex justify-between items-center">
@@ -12,11 +21,35 @@ const Header = () => {
             height={70}
           />
         </NavLink>
-        <ul className="flex space-x-4 gap-10">
+        <button
+          className="block lg:hidden text-white focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+        <ul
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          }   lg:flex flex-col md:flex-col md:gap-9 lg:flex-row lg:space-x-4 lg:gap-10 absolute lg:relative bg-black lg:bg-transparent left-0 top-16 lg:top-0 w-full lg:w-auto lg:pl-0 pl-4 transition-all duration-300`}
+        >
           <li>
             <NavLink
               to="/"
-              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554]"
+              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554] block lg:inline"
+              onClick={closeMenu}
             >
               Home
             </NavLink>
@@ -24,7 +57,8 @@ const Header = () => {
           <li>
             <NavLink
               to="/service"
-              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554]"
+              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554] block lg:inline"
+              onClick={closeMenu}
             >
               Service
             </NavLink>
@@ -32,7 +66,8 @@ const Header = () => {
           <li>
             <NavLink
               to="/blog"
-              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554]"
+              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554] block lg:inline"
+              onClick={closeMenu}
             >
               Blog
             </NavLink>
@@ -40,7 +75,8 @@ const Header = () => {
           <li>
             <NavLink
               to="/contact"
-              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554]"
+              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554] block lg:inline"
+              onClick={closeMenu}
             >
               Contact
             </NavLink>
@@ -48,7 +84,8 @@ const Header = () => {
           <li>
             <NavLink
               to="/about"
-              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554]"
+              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554] block lg:inline"
+              onClick={closeMenu}
             >
               About
             </NavLink>
@@ -56,7 +93,8 @@ const Header = () => {
           <li>
             <NavLink
               to="/property-listing"
-              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554]"
+              className="text-white p-[0.5rem] mx-0 my-[0.5rem] rounded hover:bg-[#172554] block lg:inline"
+              onClick={closeMenu}
             >
               Property Listing
             </NavLink>
